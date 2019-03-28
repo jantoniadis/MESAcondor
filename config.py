cf5f04b91276e387a4a7c6c3af16ba90ff2bac97
Savvas Chanlaridis <schanlar@astro.uni-bonn.de>
2019-03-28 19:23:37 +0100

    Updated to evolve binaries with initial HeZAMS model

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 #path to folder that contains the mesa executable
-mesa_root_dir = '/vol/aibn185/data1/jantoniadis/mesa-r10398'
-mesa_directory = '/vol/aibn185/data1/jantoniadis/mesa-r10398/star/test_suite/Condor'
-out_directory= '/vol/hal/halraid/jantoniadis/HeCores/Condor/full_grid'
-script_directory = '/vol/aibn185/data1/jantoniadis/MESAcondor'
+mesa_root_dir = '/vol/aibn1107/data2/schanlar/mesa-r10398'
+mesa_directory = '/vol/aibn1107/data2/schanlar/mesa-r10398/star/test_suite/Condor'
+out_directory= '/vol/hal/halraid/schanlar/Condor/HeZAMS'
+script_directory = '/vol/aibn1107/data2/schanlar/MESAcondor'
 
 #Can explore up to three variables
 
 variable1 = {'name': 'initial_mass',
              'location': 'inlist_var',
              'type': 'array',
-             'minimum': 1.5,
+             'minimum': 1.0,
              'maximum': 3.5,
              'step': 0.1,
 }
--- config_binary.py
+++ config_binary.py
@@ -1,22 +1,41 @@
 #path to folder that contains the mesa executable
-mesa_root_dir = '/vol/aibn185/data1/jantoniadis/mesa-r10398'
-mesa_directory = '/vol/aibn185/data1/jantoniadis/mesa-r10398/binary/test_suite/Condor'
-out_directory= '/vol/hal/halraid/jantoniadis/HeCores/Condor/tests'
-script_directory = '/vol/aibn185/data1/jantoniadis/MESAcondor'
+mesa_root_dir = '/vol/aibn1107/data2/schanlar/mesa-r10398'
+mesa_directory = '/vol/aibn1107/data2/schanlar/mesa-r10398/binary/test_suite/Condor'
+out_directory= '/vol/hal/halraid/schanlar/Condor/Binaries'
+script_directory = '/vol/aibn1107/data2/schanlar/MESAcondor'
 
+#path to folder that contains the HeZAMS models
+init_dir = '/vol/hal/halraid/schanlar/Condor/HeZAMS/NO_OVERSHOOT/*'
 #Can explore up to three variables
 
 
 
 
 import numpy as np
+import glob
+import os
 periods = 10**np.arange(-1.,3.6,0.1)
 
+HeZAMS_paths = [path for path in glob.glob(init_dir)]
+initial_model_name = []
+initial_model_path = []
+for i in HeZAMS_paths:
+	os.chdir(i)
+	temp = glob.glob('*.mod')
+	try:
+		initial_model_path.append(i+'/'+temp[0])
+		temp[0] = temp[0].replace('.mod', '')
+		initial_model_name.append(temp[0])
+	except:
+		continue
+
+initial_model_name.sort()
+initial_model_path.sort()	
 
 
 variable1 = {'name': 'm1',
              'type': 'array',
-             'minimum': 1.5,
+             'minimum': 1.0,
              'maximum': 3.5,
              'step': 0.1,
              'inlist':'inlist_project'
@@ -28,10 +47,11 @@ variable2 = {'name': 'initial_period_in_days',
              'inlist':'inlist_project'
 }
 
-variable3 = {'name': 'initial_z',
+variable3 = {'name': 'saved_model_name',
              'type': 'predetermined_array',
-             'values': [0.02],
+             'values': (initial_model_name, initial_model_path),
              'inlist':'inlist1'
 }
 
 grid_variables = [variable1,variable2,variable3]
+

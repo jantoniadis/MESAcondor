e23254a507fa4a12b272ed5b5d959ea8cd0c9c34
Savvas Chanlaridis <schanlar@astro.uni-bonn.de>
2019-04-12 19:43:48 +0200

    Updated to include binaries + runtime optimized

--- check_finish.py
+++ check_finish.py
@@ -1,27 +1,53 @@
-import re, os, glob
+import glob
 from file_read_backwards import FileReadBackwards
+import time
+import warnings
 
 condorPath = '/vol/hal/halraid/jantoniadis/HeCores/Condor/full_grid'
+#condorPath = '/vol/hal/halraid/schanlar/Condor/Binaries'
+config = 'single'
 
 
-def checkOutput(path):
+def checkOutput(path, config = 'single'):
+    '''
+    The config variable accepts either the value "single" or
+    the value "binary"
+    '''
+
+
     hasFinished = False
     keyword1 = 'stop because'
     keyword2 = 'termination'
-    name = path[-20:]
+    line_counter = 1
+
+    if config == 'single':
+        name = path[-20:]
+    elif config == 'binary':
+        idx = path.index('bin')
+        name = path[idx:]
+    else:
+        raise ValueError('config must be set either to single or binary!')
+
+
     with FileReadBackwards(f'{path}/condor.out') as file:
         for line in file:
-            if line.startswith(f'{keyword1}') or line.startswith(f'{keyword2}'):
-                current_status = f'Model {name} has been terminated!'
-                hasFinished = True
-                break
+            # check only the last 20 lines
+            if line_counter <= 20:
+                if line.startswith(f'{keyword1}') or line.startswith(f'{keyword2}'):
+                    current_status = f'Model {name} has been terminated!'
+                    hasFinished = True
+                    break
+                else:
+                    current_status = f'Model {name} still running!'
+                line_counter += 1
             else:
-                current_status = f'Model {name} still running!'
-                
+                break
     return hasFinished, current_status
 
 
 def main():
+
+    start_timer = time.time()
 
     model_paths = [path for path in glob.glob(condorPath + '/*')]
     model_paths.sort()
@@ -29,17 +55,22 @@ def main():
     finished = 0
     running = 0
     for i in model_paths:
-        hasFinished, status = checkOutput(i)
+        try:
+            hasFinished, status = checkOutput(i, config=config)
 
-        if hasFinished:
-            print(status)
-            finished += 1
-        else:
-            print(status)
-            running += 1
+            if hasFinished:
+                print(status)
+                finished += 1
+            else:
+                print(status)
+                running += 1
+        except:
+            #pass
+            warnings.warn('Run into trouble when trying to check this path', SyntaxWarning)
 
     print(f'From {len(model_paths)} models, {finished} have finished, and {running} are still running!')
-
+    stop_timer = time.time()
+    print(f'Time elapsed: {round(stop_timer - start_timer, 2)} seconds')
 
 if __name__ == '__main__':
     main()
